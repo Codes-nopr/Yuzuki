2eb57ba72a3eeebe569b2b1790cf58cff43e88ea
dragonfirefox <luhamailbox@gmail.com>
2022-01-11 22:57:25 +0530

    rawWS event for voice update

---
+++ src/Listeners/Guild/debug.js
@@ -0,0 +1,2 @@
+// eslint-disable-next-line no-console
+module.exports = (client, debug) => console.log(`[Debug] ${debug}`);
---
+++ src/Listeners/Guild/rawWS.js
@@ -0,0 +1,1 @@
+module.exports = (client, d) => client.manager.updateVoiceState(d);
