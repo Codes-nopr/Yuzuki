a0946450701ebba5a4b67d9083a206ae161b8ee7
dragonfirefox <luhamailbox@gmail.com>
2022-01-11 15:45:38 +0530

    message event and rich embed

---
+++ src/Listeners/Guild/messageCreate.js
@@ -0,0 +1,45 @@
+const RichEmbed = require("../../Utility/RichEmbed");
+
+const cooldowns = new Map();
+
+module.exports = async (client, message) => {
+    if (message.author.bot || !message.guild) return;
+
+    const regex = new RegExp(`^<@!?${client.user.id}> `);
+    const prefix = message.content.match(regex) ? message.content.match(regex)[0] : "lov ";
+
+    try {
+        if (!message.content.startsWith(prefix)) return;
+        const args = message.content.slice(prefix.length).split(/ +/g);
+        const cmd = args.shift()?.toLowerCase();
+        const command = client.commands.get(cmd)
+        || client.commands.find((x) => x.aliases && x.aliases.includes(cmd));
+
+        if (!cooldowns.has(command.name?.toLowerCase())) {
+            cooldowns.set(command.name?.toLowerCase(), new Map());
+        }
+
+        const now = Date.now();
+        const timeStamp = cooldowns.get(command.name);
+        const cooldownAmount = command.cooldown * 1000;
+
+        if (timeStamp.has(message.author.id)) {
+            const then = timeStamp.get(message.author.id) + cooldownAmount;
+
+            if (now < then) {
+                const timeLeft = (then - now) / 1000;
+                // eslint-disable-next-line consistent-return
+                return message.channel.send({ embeds: [new RichEmbed().setColor(client.color.color).setDescription(`Wooah, please wait **${timeLeft.toFixed(2)}** seconds before using this command.`)] });
+            }
+        }
+
+        timeStamp.set(message.author.id, now);
+        setTimeout(() => timeStamp.delete(message.author.id), cooldownAmount);
+
+        try {
+            command.run(client, message, args);
+        // eslint-disable-next-line no-empty
+        } catch { }
+    // eslint-disable-next-line no-empty
+    } catch { }
+};
