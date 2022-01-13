eb0c2983456a7afa54fe0fe5f8db30b5aec33b5e
dragonfirefox <luhamailbox@gmail.com>
2022-01-13 13:22:21 +0530

    dj role for loop

--- src/Commands/Music/loop.js
+++ src/Commands/Music/loop.js
@@ -1,3 +1,5 @@
+const GuildForceSkip = require("../../Models/GuildForceSkip");
+
 module.exports = {
     name: "loop",
     aliases: ["lp"],
@@ -6,34 +8,73 @@ module.exports = {
     run: async ({ client, message, args }) => {
         if (!message.member.voiceState.channelID) return message.channel.createMessage({ content: `${client.emote.error} You need to be in a voice channel before running this command.` });
 
-        const botVoice = await client.getRESTGuildMember(message.guildID, client.user.id);
+        GuildForceSkip.findOne({
+            guild: message.guildID,
+        }, async (error, resultData) => {
+            if (error) return message.channel.createMessage({ content: `${client.emote.error} Something went wrong while checking data on database.` });
+            if (resultData) {
+                if (!message.member.roles.find((x) => x === resultData.role)) return message.channel.createMessage({ content: `${client.emote.error} **${message.author.username}** you can't use forceskip without DJ role.` });
 
-        if (botVoice.voiceState.channelID) {
-            if (botVoice.voiceState.channelID !== message.member.voiceState.channelID) {
-                return message.channel.createMessage({ content: `${client.emote.error} You are not connected to the same voice channel.` });
+                const botVoice = await client.getRESTGuildMember(message.guildID, client.user.id);
+
+                if (botVoice.voiceState.channelID) {
+                    if (botVoice.voiceState.channelID !== message.member.voiceState.channelID) {
+                        return message.channel.createMessage({ content: `${client.emote.error} You are not connected to the same voice channel.` });
+                    }
+                }
+                const player = client.manager.get(message.guildID);
+                if (!player || !player.queue.current) return message.channel.createMessage({ content: `${client.emote.error} Not playing anything in voice channel.` });
+
+                if (!args[0]) return message.channel.createMessage({ content: `${client.emote.error} Please provide a mode \`track\` either \`queue\`.` });
+                switch (args[0]) {
+                    case "track":
+                        if (player.queueRepeat === true) player.setQueueRepeat(false);
+                        player.setTrackRepeat(!player.trackRepeat);
+                        message.channel.createMessage({ content: `${client.emote.repeat_track} Current track repeat has been ${player.trackRepeat ? "**enabled**" : "**disabled**"}.` });
+                    break;
+
+                    case "queue":
+                        if (player.trackRepeat === true) player.setTrackRepeat(false);
+                        player.setQueueRepeat(!player.queueRepeat);
+                        message.channel.createMessage({ content: `${client.emote.repeat_queue} Current queue repeat has been ${player.queueRepeat ? "**enabled**" : "**disabled**"}.` });
+                    break;
+
+                    default:
+                        message.channel.createMessage({ content: `${client.emote.error} You have provided a wrong mode, please provide a valid mode \`track\` either \`queue\`.` });
+                    break;
+                }
+            } else {
+                const botVoice = await client.getRESTGuildMember(message.guildID, client.user.id);
+
+                if (botVoice.voiceState.channelID) {
+                    if (botVoice.voiceState.channelID !== message.member.voiceState.channelID) {
+                        return message.channel.createMessage({ content: `${client.emote.error} You are not connected to the same voice channel.` });
+                    }
+                }
+                const player = client.manager.get(message.guildID);
+                if (!player || !player.queue.current) return message.channel.createMessage({ content: `${client.emote.error} Not playing anything in voice channel.` });
+
+                if (!args[0]) return message.channel.createMessage({ content: `${client.emote.error} Please provide a mode \`track\` either \`queue\`.` });
+                switch (args[0]) {
+                    case "track":
+                        if (player.queueRepeat === true) player.setQueueRepeat(false);
+                        player.setTrackRepeat(!player.trackRepeat);
+                        message.channel.createMessage({ content: `${client.emote.repeat_track} Current track repeat has been ${player.trackRepeat ? "**enabled**" : "**disabled**"}.` });
+                    break;
+
+                    case "queue":
+                        if (player.trackRepeat === true) player.setTrackRepeat(false);
+                        player.setQueueRepeat(!player.queueRepeat);
+                        message.channel.createMessage({ content: `${client.emote.repeat_queue} Current queue repeat has been ${player.queueRepeat ? "**enabled**" : "**disabled**"}.` });
+                    break;
+
+                    default:
+                        message.channel.createMessage({ content: `${client.emote.error} You have provided a wrong mode, please provide a valid mode \`track\` either \`queue\`.` });
+                    break;
+                }
             }
-        }
-        const player = client.manager.get(message.guildID);
-        if (!player || !player.queue.current) return message.channel.createMessage({ content: `${client.emote.error} Not playing anything in voice channel.` });
-
-        if (!args[0]) return message.channel.createMessage({ content: `${client.emote.error} Please provide a mode \`track\` either \`queue\`.` });
-        switch (args[0]) {
-            case "track":
-                if (player.queueRepeat === true) player.setQueueRepeat(false);
-                player.setTrackRepeat(!player.trackRepeat);
-                message.channel.createMessage({ content: `${client.emote.repeat_track} Current track repeat has been ${player.trackRepeat ? "**enabled**" : "**disabled**"}.` });
-            break;
-
-            case "queue":
-                if (player.trackRepeat === true) player.setTrackRepeat(false);
-                player.setQueueRepeat(!player.queueRepeat);
-                message.channel.createMessage({ content: `${client.emote.repeat_queue} Current queue repeat has been ${player.queueRepeat ? "**enabled**" : "**disabled**"}.` });
-            break;
-
-            default:
-                message.channel.createMessage({ content: `${client.emote.error} You have provided a wrong mode, please provide a valid mode \`track\` either \`queue\`.` });
-            break;
-        }
+            return null;
+        });
         return null;
     },
 };
--- src/Models/GuildLoop.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const Schema = new mongoose.Schema({
-    guild: String,
-    guildLoop: String,
-});
-
-module.exports = mongoose.model("GuildLoop", Schema);
