90677af0311a813b711930f239a52b1d089197c9
dragonfirefox <luhamailbox@gmail.com>
2022-01-14 22:51:11 +0530

    path fixed

--- src/Commands/Settings/setdjrole.js
+++ src/Commands/Settings/setdjrole.js
@@ -1,4 +1,4 @@
-const GuildForceSkip = require("../../Models/GuildForceSkip");
+const GuildDJRole = require("../../Models/GuildDJRole");
 
 module.exports = {
     name: "setdjrole",
@@ -10,19 +10,17 @@ module.exports = {
             return message.channel.createMessage({ content: `${client.emote.error} You have not permission to use this command in this server. Required Permission: \`Manage Guild\`` });
         }
 
-        const restMember = await client.getRESTGuildMember(message.guildID, message.author.id);
-
         const roleID = args[0];
         if (!roleID) return message.channel.createMessage({ content: `${client.emote.error} Please specify a role ID.` });
         if (roleID.length < 18 || roleID.length > 18) return message.channel.createMessage({ content: `${client.emote.error} Are you sure its valid role...?` });
 
-        GuildForceSkip.findOne({
+        GuildDJRole.findOne({
             guild: message.guildID,
         }, async (error, resultData) => {
             if (error) return message.channel.createMessage({ content: `${client.emote.error} Something went wrong while checking data on database.` });
             if (!resultData) {
                 // eslint-disable-next-line no-param-reassign
-                resultData = new GuildForceSkip({
+                resultData = new GuildDJRole({
                     guild: message.guildID,
                     role: roleID,
                 }).save().catch(() => message.channel.createMessage({ content: `${client.emote.error} Something went wrong while saving config to database.` }));
