c0bc50bc9d8e40d89d22513091041401bf46c9fe
dragonfirefox <luhamailbox@gmail.com>
2022-01-14 22:49:16 +0530

    ok reverted

--- src/Commands/Music/autoplay.js
+++ src/Commands/Music/autoplay.js
@@ -1,5 +1,3 @@
-const GuildDJRole = require("../../Models/GuildDJRole");
-
 module.exports = {
     name: "autoplay",
     aliases: ["au", "ap"],
@@ -7,80 +5,35 @@ module.exports = {
 
     run: async ({ client, message }) => {
         if (!message.member.voiceState.channelID) return message.channel.createMessage({ content: `${client.emote.error} You need to be in a voice channel before running this command.` });
+        const botVoiceChannel = await client.getRESTGuildMember(message.guildID, client.user.id);
 
-        GuildDJRole.findOne({
-            guild: message.guildID,
-        }, async (error, resultData) => {
-            if (error) return message.channel.createMessage({ content: `${client.emote.error} Something went wrong while checking data on database.` });
-            if (resultData) {
-                if (!message.member.roles.find((x) => x === resultData.role)) return message.channel.createMessage({ content: `${client.emote.error} **${message.author.username}** you can't use forceskip without DJ role.` });
-
-                // eslint-disable-next-line max-len
-                const botVoiceChannel = await client.getRESTGuildMember(message.guildID, client.user.id);
-
-                if (botVoiceChannel.voiceState.channelID) {
-                    // eslint-disable-next-line max-len
-                    if (botVoiceChannel.voiceState.channelID !== message.member.voiceState.channelID) {
-                        return message.channel.createMessage({ content: `${client.emote.error} You are not connected to the same voice channel.` });
-                    }
-                }
-                const player = client.manager.get(message.guildID);
-                if (!player || !player.queue.current) return message.channel.createMessage({ content: `${client.emote.error} Not playing anything in voice channel.` });
-
-                const isAutoplay = player.get("autoplay");
-                if (!isAutoplay) {
-                    const msg = await message.channel.createMessage({ content: `${client.emote.doing} Enabling autoplay, just some seconds...` });
-                    const { identifier } = player.queue.current;
-                    player.set("autoplay", true);
-                    player.set("requester", message.author);
-                    player.set("identifier", identifier);
-                    const search = `https://www.youtube.com/watch?v=${identifier}&list=RD${identifier}`;
-                    const query = await player.search(search, message.author);
-                    if (query.loadType === "LOAD_FAILED") return message.channel.createMessage({ content: `${client.emote.error} Something went wrong while loading the track.` });
-                    player.queue.add(Array.isArray(query).length > 1
-                    ? query.tracks : query.tracks[1]);
-                    await msg.edit({ content: `${client.emote.autoplay} Autoplay has been enabled.` })
-                    .catch(() => { });
-                }
-                if (isAutoplay) {
-                    player.set("autoplay", false);
-                    return message.channel.createMessage({ content: `${client.emote.autoplay} Autoplay has been disabled.` });
-                }
-                return null;
-            }
+        if (botVoiceChannel.voiceState.channelID) {
             // eslint-disable-next-line max-len
-            const botVoiceChannel = await client.getRESTGuildMember(message.guildID, client.user.id);
+            if (botVoiceChannel.voiceState.channelID !== message.member.voiceState.channelID) {
+                return message.channel.createMessage({ content: `${client.emote.error} You are not connected to the same voice channel.` });
+            }
+        }
+        const player = client.manager.get(message.guildID);
+        if (!player || !player.queue.current) return message.channel.createMessage({ content: `${client.emote.error} Not playing anything in voice channel.` });
 
-            if (botVoiceChannel.voiceState.channelID) {
-                // eslint-disable-next-line max-len
-                if (botVoiceChannel.voiceState.channelID !== message.member.voiceState.channelID) {
-                    return message.channel.createMessage({ content: `${client.emote.error} You are not connected to the same voice channel.` });
-                }
-            }
-            const player = client.manager.get(message.guildID);
-            if (!player || !player.queue.current) return message.channel.createMessage({ content: `${client.emote.error} Not playing anything in voice channel.` });
-
-            const isAutoplay = player.get("autoplay");
-            if (!isAutoplay) {
-                const msg = await message.channel.createMessage({ content: `${client.emote.doing} Enabling autoplay, just some seconds...` });
-                const { identifier } = player.queue.current;
-                player.set("autoplay", true);
-                player.set("requester", message.author);
-                player.set("identifier", identifier);
-                const search = `https://www.youtube.com/watch?v=${identifier}&list=RD${identifier}`;
-                const query = await player.search(search, message.author);
-                if (query.loadType === "LOAD_FAILED") return message.channel.createMessage({ content: `${client.emote.error} Something went wrong while loading the track.` });
-                player.queue.add(Array.isArray(query).length > 1
-                ? query.tracks : query.tracks[1]);
-                await msg.edit({ content: `${client.emote.autoplay} Autoplay has been enabled.` })
-                .catch(() => { });
-            }
-            if (isAutoplay) {
-                player.set("autoplay", false);
-                return message.channel.createMessage({ content: `${client.emote.autoplay} Autoplay has been disabled.` });
-            }
-            return null;
-        });
+        const isAutoplay = player.get("autoplay");
+        if (!isAutoplay) {
+            const msg = await message.channel.createMessage({ content: `${client.emote.doing} Enabling autoplay, just some seconds...` });
+            const { identifier } = player.queue.current;
+            player.set("autoplay", true);
+            player.set("requester", message.author);
+            player.set("identifier", identifier);
+            const search = `https://www.youtube.com/watch?v=${identifier}&list=RD${identifier}`;
+            const query = await player.search(search, message.author);
+            if (query.loadType === "LOAD_FAILED") return message.channel.createMessage({ content: `${client.emote.error} Something went wrong while loading the track.` });
+            player.queue.add(Array.isArray(query).length > 1 ? query.tracks : query.tracks[1]);
+            await msg.edit({ content: `${client.emote.autoplay} Autoplay has been enabled.` })
+            .catch(() => { });
+        }
+        if (isAutoplay) {
+            player.set("autoplay", false);
+            return message.channel.createMessage({ content: `${client.emote.autoplay} Autoplay has been disabled.` });
+        }
         return null;
     },
 };
