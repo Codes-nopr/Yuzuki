d5f94fa137ce14a4c27d6cfe545de8c02deba46d
dragonfirefox <luhamailbox@gmail.com>
2022-01-14 22:24:09 +0530

    unshuffle command

---
+++ src/Commands/Music/unshuffle.js
@@ -0,0 +1,23 @@
+module.exports = {
+    name: "unshuffle",
+    aliases: ["ushuf", "unshf"],
+    cooldown: 2,
+
+    run: async ({ client, message }) => {
+        if (!message.member.voiceState.channelID) return message.channel.createMessage({ content: `${client.emote.error} You need to be in a voice channel before running this command.` });
+
+        const botVoice = await client.getRESTGuildMember(message.guildID, client.user.id);
+
+        if (botVoice.voiceState.channelID) {
+            if (botVoice.voiceState.channelID !== message.member.voiceState.channelID) {
+                return message.channel.createMessage({ content: `${client.emote.error} You are not connected to the same voice channel.` });
+            }
+        }
+        const player = client.manager.get(message.guildID);
+        if (!player || !player.queue.current) return message.channel.createMessage({ content: `${client.emote.error} Not playing anything in voice channel.` });
+
+        player.queue.clear();
+        player.get("beforeshuffle").forEach((e) => player.queue.add(e));
+        return message.channel.createMessage({ content: `${client.emote.ok} Unshuffled **${player.queue.length}** tracks.` });
+    },
+};
